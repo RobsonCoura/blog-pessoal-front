2298e9a690923fb345ad2cbeb87eaf367403929e
Robson Coura <robson_coura@yahoo.com.br>
2021-12-20 09:21:20 -0300

    Doc blog pessoal

--- Front-end/src/app/cadastrar/cadastrar.component.ts
+++ Front-end/src/app/cadastrar/cadastrar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
+import { AlertasService } from '../service/alertas.service';
 import { AuthService } from '../service/auth.service';
 
 @Component({
@@ -17,7 +18,8 @@ export class CadastrarComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private alertas: AlertasService
   ) { }
 
   ngOnInit() {
@@ -38,12 +40,12 @@ cadastrar(){
 this.user.tipo = this.tipoUsuario
 
 if(this.user.senha != this.confirmarSenha){
-alert('A senhas estão incorretas.')
+this.alertas.showAlertDanger('A senhas estão incorretas.')
 }else{
 this.authService.cadastrar(this.user).subscribe((resp: User) => {
   this.user = resp
   this.router.navigate(['/entrar'])
-  alert('Usuário cadastrado com sucesso!')
+  this.alertas.showAlertSuccess('Usuário cadastrado com sucesso!')
 })
 }
 }
